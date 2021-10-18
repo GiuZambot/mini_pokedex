3fbdadc0bfaf0caf28a50658cd0c3b954edf7d06
Giu Zambot <giuzambot@gmail.com>
2021-10-18 20:49:54 -0300

    Mini PokeDex 0.1.1

--- src/PokeBox/index.tsx
+++ src/PokeBox/index.tsx
@@ -36,18 +36,17 @@ function Pokebox() {
     return (
         <div className="card">
             <h1>Mini - PokeDex</h1>
-            <h3>Digite o nome e de enter</h3>
-            <input type='text' onKeyPress={(e) => handleDigita(e)} />
+            <input type='text' onKeyPress={(e) => handleDigita(e)} placeholder="Digite e de enter" />
             <h2>Pokemon:</h2>
             {pokemon &&
                 <>
 
-                    <p>Nome: {pokemon.species.name}</p>
+                    <p>Nome: <strong>{pokemon.species.name}</strong></p>
                     <p>
                         Habilidades:
                         {pokemon.abilities.map((a: any) => <Hab nome={a.ability.name} handleClick={handleClick} />)}
                     </p>
-                    <img src={pokemon.sprites.front_default} alt="Foto Pokemon" />
+                    <img className="poke-img" src={pokemon.sprites.front_default} alt="Foto Pokemon" />
                 </>
             }
             {habilidade &&
