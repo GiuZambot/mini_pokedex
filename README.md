0dcaa3b8f8bbdd4210cdf03a9d9eb314c2a21ec3
Giu Zambot <giuzambot@gmail.com>
2021-10-18 20:28:44 -0300

    Mini PokeDex 0.1

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-# Getting Started with Create React App
+# Mini PokeBox - Tutorial
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Tutorial React/TypeScript/API
 
-## Available Scripts
+## Criar projeto
 
-In the project directory, you can run:
+Rodar no terminal:
+### `npx create-react-app . --template typescript`
 
-### `yarn start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
 ### `yarn test`
 
---
+++ src/PokeBox/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import './styles.css'
+
+function Pokebox() {
+    const [pokemon, setPokemon] = useState<any>();
+    const [habilidade, sethabilidade] = useState<any>();
+
+    const handleDigita = async (e: any) => {
+        if (e.code !== 'Enter') return;
+        try {
+            const pokeName: string = e.target.value;
+            const pokeData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+            const pokeObj = await pokeData.json();
+            setPokemon(pokeObj);
+        } catch (error) {
+            alert("Pokemon não encontrado");
+        }
+    }
+
+    const handleClick = async (nome: string) => {
+        try {
+            const pokeData = await fetch(`https://pokeapi.co/api/v2/ability/${nome}`);
+            const pokeObj = await pokeData.json();
+            console.log(pokeObj);
+
+            sethabilidade(pokeObj);
+        } catch (error) {
+            alert("Habilidade não encontrado");
+        }
+    }
+
+    const handleModal = () => {
+        sethabilidade(null);
+    }
+
+    return (
+        <div className="card">
+            <h1>Mini - PokeDex</h1>
+            <h3>Digite o nome e de enter</h3>
+            <input type='text' onKeyPress={(e) => handleDigita(e)} />
+            <h2>Pokemon:</h2>
+            {pokemon &&
+                <>
+
+                    <p>Nome: {pokemon.species.name}</p>
+                    <p>
+                        Habilidades:
+                        {pokemon.abilities.map((a: any) => <Hab nome={a.ability.name} handleClick={handleClick} />)}
+                    </p>
+                    <img src={pokemon.sprites.front_default} alt="Foto Pokemon" />
+                </>
+            }
+            {habilidade &&
+                <div className="hidden" onClick={handleModal}>
+                    {habilidade.nome} - {habilidade.effect_entries[1].effect}
+                </div>
+
+            }
+        </div>
+    );
+}
+
+export function Hab({ nome, handleClick }: { nome: string, handleClick: Function }) {
+    return (
+        <span className="habilidadeBtn" onClick={() => handleClick(nome)}>{nome}</span>
+    )
+}
+
+export default Pokebox;
